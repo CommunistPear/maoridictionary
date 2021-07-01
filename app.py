0f8cff996f7c18a47ad8de12cb97e9b2e080ce8c
CommunistPear <caelan@main.co.nz>
2021-07-01 13:18:20 +1200

    Table stuff 3

--- app.py
+++ app.py
@@ -10,6 +10,44 @@ bcrypt = Bcrypt(app)
 app.secret_key = "sefofe@#$%**jkvbuseb22BUJBBOPVIUBZPuboiserfbuso@#@##21ashjsrf77755kl%^$##"
 
 
+@app.route('/login', methods=["GET", "POST"])
+def render_login_page():
+    if is_logged_in():
+        return redirect('/')
+    print(request.form)
+    if request.method == "POST":
+        email = request.form['email'].strip().lower()
+        password = request.form['password'].strip()
+
+        query = """SELECT id, fname, password FROM customers WHERE email = ?"""
+        con = create_connection(DB_NAME)
+        cur = con.cursor()
+        cur.execute(query, (email,))
+        user_data = cur.fetchall()
+        con.close()
+        # if given the email is not in the database this will raise an error
+        # would be better to find out how to see if the query return an empty resultset
+        try:
+            userid = user_data[0][0]
+            firstname = user_data[0][1]
+            db_password = user_data[0][2]
+        except IndexError:
+            return redirect("/login?error=Email+invalid+or+password+incorrect")
+
+        # check if the password is incorrect for that email address
+
+        if not bcrypt.check_password_hash(db_password, password):
+            return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
+
+        session['email'] = email
+        session['userid'] = userid
+        session['firstname'] = firstname
+        session['cart'] = []
+        print(session)
+        return redirect('/')
+
+    return render_template('login.html', logged_in=is_logged_in())
+
 
 def create_connection(db_file):
     # creates connection to database
@@ -18,17 +56,6 @@ def create_connection(db_file):
         return connection
     except Error as e:
         print(e)
-
-
- def get_words():
-     con = create_connection(DB_NAME)
-     query = "SELECT id, words" \
-             " FROM Insert ORDER BY * ASC"
-     cur = con.cursor()  # You need this line next
-     cur.execute(query)  # this line actually executes the query
-     class_list = cur.fetchall()  # puts the results into a list usable in python
-    con.close()
-    return words
 
 
 @app.route('/')
