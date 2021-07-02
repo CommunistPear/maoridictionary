973a597b44eaeb1d46ec3f1be8b5ccba8d47dcb4
CommunistPear <caelan@main.co.nz>
2021-07-02 12:44:55 +1200

    2/07/21 12:44

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, request
 import sqlite3
 from sqlite3 import Error
 from flask_bcrypt import Bcrypt
@@ -19,7 +19,7 @@ def render_login_page():
         email = request.form['email'].strip().lower()
         password = request.form['password'].strip()
 
-        query = """SELECT id, fname, password FROM customers WHERE email = ?"""
+        query = """SELECT id, f_name, password FROM users WHERE email = ?"""
         con = create_connection(DB_NAME)
         cur = con.cursor()
         cur.execute(query, (email,))
@@ -49,19 +49,116 @@ def render_login_page():
     return render_template('login.html', logged_in=is_logged_in())
 
 
+@app.route('/signup', methods=['GET', 'POST'])
+def render_signup_page():
+    if is_logged_in():
+        return redirect('/')
+
+    if request.method == 'POST':
+        print(request.form)
+        f_name = request.form.get('f_name').strip().title()
+        l_name = request.form.get('l_name').strip().title()
+        email = request.form.get('email').strip().lower()
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+
+        if password != password2:
+            return redirect('/signup?error=Passwords+dont+match')
+
+        if len(password) < 8:
+            return redirect('/signup?error=Password+must+be+8+characters+or+more')
+
+        hashed_password = bcrypt.generate_password_hash(password)
+
+        con = create_connection(DB_NAME)
+
+        query = "INSERT INTO users (id, f_name, l_name, email, password) " \
+                "VALUES(NULL,?,?,?,?)"
+
+        cur = con.cursor()  # You need this line next
+        try:
+            cur.execute(query, (f_name, l_name, email, hashed_password))  # this line actually executes the query
+        except sqlite3.IntegrityError:
+            return redirect('/signup?error=Email+is+already+used')
+
+        con.commit()
+        con.close()
+        return redirect('/login')
+
+    return render_template('signup.html', logged_in=is_logged_in())
+
+
+@app.route('/logout')
+def logout():
+    print(list(session.keys()))
+    [session.pop(key) for key in list(session.keys())]
+    print(list(session.keys()))
+    return redirect('/?message=See+you+next+time!')
+
+
+def is_logged_in():
+    if session.get("email") is None:
+        print("not logged in")
+        return False
+    else:
+        print("logged in")
+        return True
+
+
 def create_connection(db_file):
-    # creates connection to database
+    """create a connection to the sqlite db - maori.db"""
     try:
         connection = sqlite3.connect(db_file)
         return connection
     except Error as e:
         print(e)
 
+    return None
+
+
+def fetch_categories():
+    con = create_connection(DB_NAME)
+    query = "SELECT id, category_name FROM category"
+    cur = con.cursor()
+    cur.execute(query)
+    categories = cur.fetchall()
+    con.close()
+    return categories
+
 
 @app.route('/')
-def render_home():
-    return render_template('home.html')
+def render_homepage():
+    print("loading home page")
+    return render_template('home.'
+                           'html', logged_in=is_logged_in(), categories=fetch_categories())
+
+
+@app.route('/categories/<cat_id>')
+def render_categories(cat_id):
+    con = create_connection(DB_NAME)
+    query = "SELECT maori, english, definition, difficulty_level, images FROM words WHERE category=?" \
+        # "ORDER BY maori_word ASC"
+    cur = con.cursor()
+    cur.execute(query, (cat_id,))
+    definitions = cur.fetchall()
+    print(definitions)
+    con.close()
+    return render_template('categories.html', definitions=definitions, logged_in=is_logged_in(),
+                           categories=fetch_categories())
+
+
+@app.route('/word/<word_id>')
+def render_detail(word_id):
+    con = create_connection(DB_NAME)
+    query = "SELECT maori, english, definition, difficulty_level, images FROM words WHERE id=?"
+    cur = con.cursor()
+    cur.execute(query, (word_id,))
+    definitions = cur.fetchall()
+    definition = definitions[0]
+    con.close()
+    return render_template('detail.html', definition=definition, logged_in=is_logged_in(),
+                           categories=fetch_categories())
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
