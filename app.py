fd0784f48dae5bb097d67f2e34432f57b827f6ce
CommunistPear <caelan@main.co.nz>
2021-07-28 22:45:24 +1200

    28/07/21 22:45

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import urllib.parse
 from flask import Flask, render_template, session, redirect, request
 import sqlite3
 from sqlite3 import Error
 from flask_bcrypt import Bcrypt
 
-DB_NAME = 'C:/Users/17075/OneDrive - Wellington College/Technology/DTS/Y13/Maori Dictionary/Maori ' \
-          'Dictionary/dictionary.db '
+# DB_NAME = 'C:/Users/17075/OneDrive - Wellington College/Technology/DTS/Y13/Maori Dictionary/Maori ' \
+#          'Dictionary/dictionary.db '
+DB_NAME = 'C:/Users/caela/Documents/School/WC/13DTS/maoridictionary/dictionary.db'
+
 app = Flask(__name__)
 
 bcrypt = Bcrypt(app)
@@ -122,7 +125,7 @@ def fetch_categories():
     con = create_connection(DB_NAME)
     print(DB_NAME)
     print(con)
-    query = "SELECT id, category_name FROM category"
+    query = "SELECT id, category_name FROM category order by category_name"
     cur = con.cursor()
     cur.execute(query)
     categories = cur.fetchall()
@@ -133,26 +136,66 @@ def fetch_categories():
 @app.route('/', methods=["GET", "POST"])
 def render_homepage():
     if request.method == "POST" and is_logged_in():
-        category_name = request.form.get('category').strip().lower()
-        print(category_name)
-        if len(category_name) < 3:
-            return redirect("/?error=Name+must+be+at+least+3+letters+long.")
-        else:
-            # connect to the database
-            con = create_connection(DB_NAME)
+        if request.form.get('form') == 'category':
+            category_name = request.form.get('category').strip().lower()
+            print(category_name)
+            if len(category_name) < 3:
+                return redirect("/?error=Name+must+be+at+least+3+letters+long.")
+            else:
+                # connect to the database
+                con = create_connection(DB_NAME)
 
-            query = "INSERT INTO category (id, category_name) VALUES(NULL, ?)"
+                query = "INSERT INTO category (id, category_name) VALUES(NULL, ?)"
 
-            cur = con.cursor()  # You need this line next
-            try:
-                cur.execute(query, (category_name,))  # this line actually executes the query
-            except:
-                return redirect('/menu?error=Unknown+error')
+                cur = con.cursor()  # You need this line next
+                try:
+                    cur.execute(query, (category_name,))  # this line actually executes the query
+                except:
+                    return redirect('/menu?error=Unknown+error')
 
-            con.commit()
-            con.close()
+                con.commit()
+                con.close()
 
-        return redirect('/?message=Thank+you+for+creating+a+category!')
+            return redirect("/?message=" + urllib.parse.quote("Thank you for creating a category!"))
+
+        if request.form.get('form') == 'word':
+            maori_word = request.form.get('maori_word').strip().lower()
+            english_word = request.form.get('english_word').strip().lower()
+            definition = request.form.get('definition').strip().lower()
+            category = request.form.get('category').strip()
+            difficulty_level = request.form.get('difficulty_level').strip().lower()
+
+            min_length = 2
+            if len(maori_word) < min_length:
+                return redirect(
+                    "/?error=" + urllib.parse.quote("Word name must be at least " + str(min_length) + " letters long."))
+            elif len(english_word) < min_length:
+                return redirect(
+                    "/?error=" + urllib.parse.quote("Word name must be at least " + str(min_length) + " letters long."))
+            elif len(definition) < min_length:
+                return redirect("/?error=" + urllib.parse.quote(
+                    "definition must be at least " + str(min_length) + " letters long."))
+            elif not category.isnumeric():
+                return redirect("/?error=" + urllib.parse.quote("Get out of my HTML."))
+            elif not difficulty_level.isnumeric() and 1 > int(difficulty_level) > 10:
+                return redirect("/?error=" + urllib.parse.quote("Difficulty must be between 1 and 10."))
+            else:
+                # connect to the database
+                con = create_connection(DB_NAME)
+
+                query = "INSERT INTO words (id, maori, english, category, definition, difficulty_level) VALUES(NULL, ?, ?, ?, ?, ?)"
+
+                cur = con.cursor()  # You need this line next
+                try:
+                    cur.execute(query, (maori_word, english_word, category, definition,
+                                        difficulty_level))  # this line actually executes the query
+                except:
+                    return redirect("/menu?error=" + urllib.parse.quote("Unknown error"))
+
+                con.commit()
+                con.close()
+
+            return redirect("/?message=" + urllib.parse.quote("Thank you for adding a word"))
 
     print("loading home page")
     return render_template('home.'
@@ -162,7 +205,7 @@ def render_homepage():
 @app.route('/categories/<cat_id>')
 def render_categories(cat_id):
     con = create_connection(DB_NAME)
-    query = "SELECT id, maori, english, definition, difficulty_level, images FROM words WHERE category=?" \
+    query = "SELECT id, maori, english, definition, difficulty_level, images FROM words WHERE category=? " \
         # "ORDER BY maori_word ASC"
     cur = con.cursor()
     cur.execute(query, (cat_id,))
@@ -178,7 +221,7 @@ def render_categories(cat_id):
 @app.route('/word/<word_id>')
 def render_detail(word_id):
     con = create_connection(DB_NAME)
-    query = "SELECT maori, english, definition, difficulty_level, images FROM words WHERE id=?"
+    query = "SELECT id, maori, english, definition, difficulty_level, images FROM words WHERE id=?"
     cur = con.cursor()
     cur.execute(query, (word_id,))
     definitions = cur.fetchall()
